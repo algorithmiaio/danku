ca5d40061bda763bf2f460bb008175175c9c6e18
A. Besir Kurtulmus <besirkurtulmus@gmail.com>
2018-01-02 02:44:51 -0800

    Add a few sample danku tests

--- tests/test_danku.py
+++ tests/test_danku.py
@@ -1,2 +1,32 @@
-def test_true():
-    assert 1 == 1
+def test_python_solidity_hashing_compatability():
+    assert(False)
+
+def test_dataset_partitioning():
+    assert(False)
+
+def test_train_neural_network():
+    assert(False)
+
+def test_danku_init():
+    assert(False)
+
+def test_danku_model_submission():
+    assert(False)
+
+def test_danku_reveal_test_data():
+    assert(False)
+
+def test_danku_dont_reveal_test_data():
+    assert(False)
+
+def test_danku_evaluate_model():
+    assert(False)
+
+def test_danku_cancel_contract:
+    assert(False)
+
+def test_danku_finalize_contract():
+    assert(False)
+
+def test_danku_model_accuracy():
+    assert(False)
