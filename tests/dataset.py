24593129a3a326aba5b74e223b15b6246693c067
A. Besir Kurtulmus <besir@algorithmia.com>
2018-01-02 11:09:39 -0800

    Add max_num_data_groups to sample datasets

--- tests/dataset.py
+++ tests/dataset.py
@@ -80,6 +80,8 @@ class SampleCircleDataset(Dataset):
         (1,0,0),(2,0,0),(3,0,0),(-2,-1,0),(-1,-1,0),(0,-1,0),(1,-1,0),(2,-1,0),
         (-1,-2,0),(0,-2,0),(1,-2,0),(0,-3,0),(-1,6,1),(1,6,1),(-1,-6,1),
         (1,-6,1),(6,1,1)]
+        self.max_num_data_groups = len(self.data)
+        assert(self.max_num_data_groups % 5 == 0)
 
 class SampleSwirlDataset(Dataset):
     '''
@@ -113,6 +115,8 @@ class SampleSwirlDataset(Dataset):
         (-1,4,1),(0,4,1),(1,4,1),(2,4,1),(3,4,1),(4,4,1),(5,4,1),(5,3,1),
         (5,2,1),(5,1,1),(5,0,1),(5,-1,1),(5,-2,1),(5,-3,1),(5,-4,1),(5,-5,1),
         (5,-6,1),(4,-6,1),(3,-6,1),(2,-6,1),(1,-6,1),(0,-6,1)]
+        self.max_num_data_groups = len(self.data)
+        assert(self.max_num_data_groups % 5 == 0)
 
 class SampleHalfDividedDataset(Dataset):
     '''
@@ -141,6 +145,8 @@ class SampleHalfDividedDataset(Dataset):
         (6,-1,0),(4,-2,0),(2,-3,0),(3,-3,0),(6,-3,0),(0,-4,0),(1,-4,0),(4,-4,0),
         (-1,-5,0),(3,-5,0),(5,-5,0),(0,-6,0),(6,-6,0),(-6,6,1),(-6,0,1),
         (-6,-1,1),(2,6,1),(2,-6,0)]
+        self.max_num_data_groups = len(self.data)
+        assert(self.max_num_data_groups % 5 == 0)
 
 class SampleAcrossCornerDataset(Dataset):
     '''
@@ -169,3 +175,5 @@ class SampleAcrossCornerDataset(Dataset):
         (4,-4,0),(5,-4,0),(6,-4,0),(3,-5,0),(4,-5,0),(5,-5,0),(6,-5,0),(4,-6,0),
         (5,-6,0),(6,-6,0),(-2,3,0),(-3,2,0),(3,-2,0),(2,-3,0),(2,3,1),(3,2,1),
         (-3-2,1),(-2,-3,1)]
+        self.max_num_data_groups = len(self.data)
+        assert(self.max_num_data_groups % 5 == 0)
