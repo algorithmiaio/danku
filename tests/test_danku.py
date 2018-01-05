48ca76d445449e569a3f0b98e3a720fb90b5c086
A. Besir Kurtulmus <besir@algorithmia.com>
2018-01-05 09:47:31 -0800

    Add block tracking and debugging info in test

--- tests/test_danku.py
+++ tests/test_danku.py
@@ -1,5 +1,6 @@
 from dutils.dataset import SampleCircleDataset
 import dutils.debug as dbg
+from secrets import randbelow
 
 def test_python_solidity_hashing_compatability():
     # Make sure Python and solidity hashes the data groups in the same manner
@@ -50,13 +51,23 @@ def test_danku_init(web3, chain):
     dbg.dprint("Hashed Hex data groups: " +
         str(list(map(lambda x: "0x" + x.hex(), scd.hashed_data_group))))
 
+    # Keep track of all block numbers, so we can send them in time
+    # Start at a random block between 0-1000
+    chain.wait.for_block(randbelow(1000))
+    dbg.dprint("Starting block: " + str(web3.eth.blockNumber))
     init1_tx = danku.transact().init1(scd.hashed_data_group, accuracy_criteria,\
         submission_t, evaluation_t, test_reveal_t)
     chain.wait.for_receipt(init1_tx)
+    init1_block_number = web3.eth.blockNumber
+    dbg.dprint("Init1 block: " + str(init1_block_number))
 
     # Initialization step 2
     # Get data group indexes
+    chain.wait.for_block(init1_block_number + 1)
     dgi = []
+    init2_block_number = web3.eth.blockNumber
+    dbg.dprint("Init2 block: " + str(init2_block_number))
+
     for i in range(scd.num_data_groups):
         dgi.append(i)
 
@@ -95,6 +106,9 @@ def test_danku_init(web3, chain):
         start = i*scd.dps*scd.partition_size
         end = start + scd.dps*scd.partition_size
         dbg.dprint("(" + str(testing_partition[i]) + ") Test data,nonce: " + str(test_data[start:end]) + "," + str(scd.test_nonce[i]))
+
+    init3_block_number = web3.eth.blockNumber
+    dbg.dprint("Init3 block: " + str(init3_block_number))
     assert(False)
 
 def test_danku_model_submission():
