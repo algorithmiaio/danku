1f323bae98686c71c4ef341248597d81694dec28
A. Besir Kurtulmus <besirkurtulmus@gmail.com>
2018-01-08 01:33:02 -0800

    Add tests for packing and unpacking weights

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -118,3 +118,46 @@ def test_train_5lnn():
     assert(not isinstance(nn.tf_weights, type(None)))
     assert(not isinstance(nn.tf_init, type(None)))
     assert(not isinstance(nn.tf_layers, type(None)))
+
+def test_packing_weights():
+    il_nn = 2
+    hl_nn = [2,3]
+    ol_nn = 2
+    nn = NeuralNetwork(il_nn, hl_nn, ol_nn)
+    weights = [[[1.9911426, -1.2063], [1.9911426, -1.2063]],\
+        [[0.23531701, 2.9445081], [0.23531701, 2.9445081],\
+        [0.23531701, 2.9445081]], [[0.065952107, 0.82053429, -0.21171442],\
+        [0.065952107, 0.82053429, -0.21171442]]]
+
+    expected_packed_weights = [1.9911426, -1.2063, 1.9911426, -1.2063,\
+        0.23531701, 2.9445081, 0.23531701, 2.9445081, 0.23531701, 2.9445081,\
+        0.065952107, 0.82053429, -0.21171442, 0.065952107, 0.82053429,\
+        -0.21171442]
+
+    packed_weights = nn.pack_weights(weights)
+
+    for v in range(len(packed_weights)):
+        assert(packed_weights[v] == expected_packed_weights[v])
+
+def test_unpacking_weights():
+    il_nn = 2
+    hl_nn = [2,3]
+    ol_nn = 2
+    nn = NeuralNetwork(il_nn, hl_nn, ol_nn)
+    packed_weights = [1.9911426, -1.2063, 1.9911426, -1.2063, 0.23531701,\
+    2.9445081, 0.23531701, 2.9445081, 0.23531701, 2.9445081, 0.065952107,\
+    0.82053429, -0.21171442, 0.065952107, 0.82053429, -0.21171442]
+
+    expected_unpacked_weights = [[[1.9911426, -1.2063], [1.9911426, -1.2063]],\
+        [[0.23531701, 2.9445081], [0.23531701, 2.9445081],\
+        [0.23531701, 2.9445081]], [[0.065952107, 0.82053429, -0.21171442],\
+        [0.065952107, 0.82053429, -0.21171442]]]
+
+    unpacked_weights = nn.unpack_weights(packed_weights, il_nn, hl_nn, ol_nn)
+
+    # g for group, d for data point and v for value
+    for g in range(len(unpacked_weights)):
+        for d in range(len(unpacked_weights[g])):
+            for v in range(len(unpacked_weights[g][d])):
+                assert(unpacked_weights[g][d][v] ==\
+                    expected_unpacked_weights[g][d][v])
