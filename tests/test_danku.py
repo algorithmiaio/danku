636703d6c86f69effb1a0c059d8f2bb7a091af1b
A. Besir Kurtulmus <besir@algorithmia.com>
2018-02-22 15:15:22 -0800

    Remove tests that were already implemented in the main tests

--- tests/test_danku.py
+++ tests/test_danku.py
@@ -673,39 +673,3 @@ def test_failed_contract_cancellation(web3, chain):
 
     # Verify the offer account didn't get refunded the reward amount
     assert bal == 999998999999999999978960
-
-'''
-def test_python_solidity_hashing_compatability():
-    # Make sure Python and solidity hashes the data groups in the same manner
-    assert(False)
-
-def test_dataset_partitioning():
-    assert(False)
-
-def test_train_single_layer_neural_network():
-    assert(False)
-
-def test_train_multi_layer_neural_network():
-    assert(False)
-
-def test_danku_model_submission():
-    assert(False)
-
-def test_danku_reveal_test_data():
-    assert(False)
-
-def test_danku_dont_reveal_test_data():
-    assert(False)
-
-def test_danku_evaluate_model():
-    assert(False)
-
-def test_danku_cancel_contract():
-    assert(False)
-
-def test_danku_finalize_contract():
-    assert(False)
-
-def test_danku_model_accuracy():
-    assert(False)
-'''
